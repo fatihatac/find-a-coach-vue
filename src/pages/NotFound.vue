09491615b8b606c31b52d317488c11886a9af858
fatihatac <fatac2000@gmail.com>
2023-03-08 23:19:00 +0300

    initial commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <base-card>
+    <h2>Page not found</h2>
+    <p>
+      This page could not be found - maybe check out all out
+      <router-link to="/coaches">coaches</router-link>
+    </p></base-card
+  >
+</template>
